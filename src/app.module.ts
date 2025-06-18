d4bfd8fb8486e3b7b05f805112b0d882f477ead6
Fabio Saito <fabio.h.saito@gmail.com>
2025-06-18 17:31:02 -0300

    chore: initial project setup and basic configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,46 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MulterModule } from '@nestjs/platform-express';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { LogParserController } from './events/application/event.controller';
+import { LogHandlerService } from './services/log-handler.service';
+import { HandleKillService } from './events/domain/service/handle-kill.service';
+import { TimestampService } from './services/timestamp.service';
+import { Match } from './matches/match.entity';
+import { Player } from './players/player.entity';
+import { Event } from './events/event.entity';
+import { Award } from './awards/award.entity';
+import { PlayerMatchStats } from './player-match-stats/player-match-stats.entity';
+import { MatchService } from './matches/domain/services/match.service';
+import { PlayerService } from './players/domain/service/player.service';
+import { PlayerMatchStatsService } from './player-match-stats/domain/service/player-match-stats.service';
+import { EventService } from './events/domain/service/event.service';
+import { MatchController } from './matches/application/match.controller';
+import { PlayerMatchStatsController } from './player-match-stats/application/player-match-stats.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MulterModule.register({
+      // dest: './uploads',
+      // storage: undefined,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [Match, Player, Event, Award, PlayerMatchStats],
+      synchronize: true, // Only for development
+      // logging: true,
+    }),
+    TypeOrmModule.forFeature([Match, Player, Event, Award, PlayerMatchStats]),
+  ],
+  controllers: [LogParserController, MatchController, PlayerMatchStatsController],
+  providers: [
+    LogHandlerService,
+    HandleKillService,
+    TimestampService,
+    MatchService,
+    PlayerService,
+    PlayerMatchStatsService,
+    EventService
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { existsSync, unlinkSync } from 'fs';
+import { ValidationPipe } from '@nestjs/common';
 declare const module: any;
 
 async function bootstrap() {
+  const dbFile = 'db.sqlite';
+  if (existsSync(dbFile)) unlinkSync(dbFile);
+
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+    whitelist: true
+  }));
+
   await app.listen(process.env.PORT ?? 3000);
 
   if (module.hot) {
